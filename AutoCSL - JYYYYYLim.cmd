d0b7d1e94446d8b601e3488df3ae30f1f2810b14
JYYYYYLim <16880388+JYYYYYLim@users.noreply.github.com>
2018-07-08 23:56:26 +0800

    v1.6
    holy fuck this is actually hard

--- AutoCSL - JYYYYYLim.cmd
+++ AutoCSL - JYYYYYLim.cmd
@@ -1,57 +1,60 @@
 @echo off
 @setlocal enableextensions enabledelayedexpansion
-color a & title AutoCSL v1.5b by JY
-echo: &  echo =================================================================================== & echo: & echo AutoCSL - release 1.5b
-
-echo: &  echo =================================================================================== & echo:
-echo LMAO so you want it the easy way eh? & goto startup
-:startup
+set "ver=1.6b"
+color a & title AutoCSL v%ver% by JY
+echo: &  echo =================================================================================== & echo: & echo AutoCSL - release %ver% & echo: &  echo =================================================================================== & echo: & echo LMAO so you want it the easy way eh? 
+if NOT exist config.cmd (
+	echo config.cmd not found. redirecting to config page...
+	goto :setup	
+)
+call config.cmd
+if /i "%browser%" EQU "cr" goto :chrome
+if /i "%browser%" EQU "ed" goto :edge
+if /i "%browser%" EQU "none" goto :startup
+:chrome
+start chrome.exe diep.io & goto :startup
+:edge
+start microsoft-edge:https://diep.io & goto :startup
 :redo
+call config.cmd
+:startup
 echo: &  echo =================================================================================== & echo:
-set /p lk=Link: (Paste the link WITHOUT https:// or http://)
+set /p lk=link: (paste the link WITHOUT https:// or http://)
 :tdm
 echo: &  echo =================================================================================== & echo: & echo 1 - 2TDM & echo 2 - 4TDM & echo:
-set /p mode=Gamemode: (type in the corresponding number.)
+set /p mode=gamemode: (type in the corresponding number.)
 if /i "%mode%" EQU "1" goto :2t
-if /i "%mode%" EQU "2" goto :2t
 if /i "%mode%" EQU "2" goto :4t
-if /i "%mode%" EQU "4" goto :4t
-echo: & echo Invalid response.
+echo: & echo invalid response.
 goto :tdm
 :2t
-set "mode=2TDM"
-goto :te
+set "mode=2TDM" & goto :te
 :4t
-set "mode=4TDM"
-goto :te
+set "mode=4TDM" & goto :te
 :te
 echo:  & echo =================================================================================== & echo: & echo 1 - Blue  & echo 2 - Purple  & echo 3 - Green  & echo 4 - Red & echo:
-set /p team=Team: (Type in the number corresponding to the team color.)
+set /p team=team: (type in the number corresponding to the team color.)
 if /i "%team%" EQU "1" goto :1
-if /i "%team%" EQU "blue" goto :1
+if /i "%team%" EQU "b" goto :1
 if /i "%team%" EQU "2" goto :2
-if /i "%team%" EQU "purple" goto :2
+if /i "%team%" EQU "p" goto :1
 if /i "%team%" EQU "3" goto :3
-if /i "%team%" EQU "green" goto :3
+if /i "%team%" EQU "g" goto :1
 if /i "%team%" EQU "4" goto :4
-if /i "%team%" EQU "red" goto :4
-echo: & echo Invalid response.
+if /i "%team%" EQU "r" goto :1
+echo: & echo invalid response.
 goto :te
 :1
-set "team=Blue"
-goto :reg
+set "team=Blue" & goto :reg
 :2
-set "team=Purple"
-goto :reg
+set "team=Purple" & goto :reg
 :3
-set "team=Green"
-goto :reg
+set "team=Green" & goto :reg
 :4
-set "team=Red"
-goto :reg
+set "team=Red" & goto :reg
 :reg
-echo: & echo =================================================================================== & echo:  & echo dl - linode-dallas & echo mi - vultr-miami & echo la - vultr-la & echo fr - linode-fremont & echo ld - linode-london  &  echo am - vultr-amsterdam & echo ff - linode-frankfurt  & echo tk - vultr-tokyo  & echo sg - vultr-singapore & echo sy - vultr-sydney & echo:
-set /p loc=Location: 
+echo: & echo =================================================================================== & echo:  & echo dl - linode-dallas *& echo mi - vultr-miami & echo la - vultr-la & echo fr - linode-fremont *& echo ld - linode-london  *& echo am - vultr-amsterdam & echo ff - linode-frankfurt  *& echo tk - vultr-tokyo  *& echo sg - vultr-singapore & echo sy - vultr-sydney & echo: & echo * - obsolete & echo:
+set /p loc=location: (enter the code corresponding to the server location.)
 if /i "%loc%" EQU "dl" goto :dallas
 if /i "%loc%" EQU "mi" goto :miami
 if /i "%loc%" EQU "la" goto :la
@@ -65,56 +68,36 @@ if /i "%loc%" EQU "sy" goto :sydney
 echo: & echo Invalid response!
 goto :reg
 :dallas
-set "loc=linode-dallas"
-set "mention=@US Notify"
-goto :status
+set "loc=linode-dallas" & set "mention=@US Notify" & goto :status
 :miami
-set "loc=vultr-miami"
-set "mention=@US Notify"
-goto :status
+set "loc=vultr-miami" & set "mention=@US Notify" & goto :status
 :la
-set "loc=vultr-la"
-set "mention=@US Notify"
-goto :status
+set "loc=vultr-la" & set "mention=@US Notify" & goto :status
 :fremont
-set "loc=linode-fremont"
-set "mention=@US Notify"
-goto :status
+set "loc=linode-fremont" & set "mention=@US Notify" & goto :status
 :london
-set "loc=linode-london"
-set "mention=@EU Notify"
-goto :status
+set "loc=linode-london" & set "mention=@EU Notify" & goto :status
 :amsterdam
-set "loc=vultr-amsterdam"
-set "mention=@EU Notify"
-goto :status
+set "loc=vultr-amsterdam" & set "mention=@EU Notify" & goto :status
 :frankfurt
-set "loc=linode-frankfurt"
-set "mention=@EU Notify"
-goto :status
+set "loc=linode-frankfurt" & set "mention=@EU Notify" & goto :status
 :tokyo
-set "loc=vultr-tokyo"
-set "mention=@Asia^/Oceania Notify"
-goto :status
+set "loc=vultr-tokyo" & set "mention=@Asia^/Oceania Notify" & goto :status
 :singapore
-set "loc=vultr-singapore" 
-set "mention=@Asia^/Oceania Notify"
-goto :status
+set "loc=vultr-singapore"  & set "mention=@Asia^/Oceania Notify" & goto :status
 :sydney
-set "loc=vultr-sydney"
-set "mention=@Asia^/Oceania Notify"
-goto :status
+set "loc=vultr-sydney" & set "mention=@Asia^/Oceania Notify" & goto :status
 :status
 echo: & echo =================================================================================== & echo: 
 set /p stt=Enter your server status.
 echo: & echo =================================================================================== & echo: 
-set /p uuid=Enter the CSL ID. (4 digit number)
-echo: & echo =================================================================================== & echo:  & echo Link: https:^/^/%lk% & echo Gamemode:  %mode% & echo Team: %team% & echo Location: %loc% & echo Status: %stt% & echo ID: %uuid% & echo Scoreboard: &  echo ^<image^> & echo: & echo Is this correct? & echo:
+set /p uuid=Enter the CSL ID. 
+echo: & echo =================================================================================== & echo:  & echo Link: https:^/^/%lk% & echo Gamemode:  %mode% & echo Team: %team% & echo Location: %loc% & echo Status: %stt% & echo ID: %uuid% & echo Scoreboard: &  echo ^<image^> & echo: & echo is this correct? & echo:
 set /p ch=(Y/N)
 if /i "%ch%" EQU "Y" goto :ok
 if /i "%ch%" EQU "N" goto :redo
 :ok
-echo:  & echo =================================================================================== & echo: & echo Result is written to output.txt in %~dp0.
+echo:  & echo =================================================================================== & echo: & echo result is written to output.txt in %~dp0.
 echo.> output.txt
 echo =================================================================================== >> output.txt
 echo.>> output.txt
@@ -139,6 +122,55 @@ echo **Status:** %stt% >> raw.txt
 echo **ID:** %uuid% >> raw.txt
 echo **Scoreboard:** >> raw.txt
 type raw.txt | clip
-echo Saved result to system clipboard.
-echo Done^^! Press any key to exit. & pause >nul
-
+echo done^^!
+if /i "%doLoop%" EQU "0" goto :exit
+if /i "%doLoop%" EQU "1" goto :loop
+if /i "%doLoop%" EQU "2" goto :pause
+:exit
+exit
+:loop
+goto :redo
+:pause
+echo press any key to exit. & pause >nul & exit
+:setup
+echo: &  echo =================================================================================== & echo: & echo - C O N F I G - & echo: &  echo =================================================================================== & echo:
+:brows
+echo - Chrome & echo - Edge & echo - None & echo:
+set /p browser=select your default startup browser. Type "none" if you have an unsupported browser/don't want autostart.
+if /i "%browser%" EQU "chrome" goto :cr
+if /i "%browser%" EQU "edge" goto :ed
+if /i "%browser%" EQU "none" goto :nu
+echo: & echo invalid response!
+goto :brows
+:cr
+set "browser=cr" & goto :stloop
+:ed
+set "browser=ed" & goto :stloop
+:nu
+set "browser=none" & goto :stloop
+:stloop
+echo: &  echo =================================================================================== & echo:
+echo exit - exit immediately & echo loop - loop. useful when generating multiple csls. & echo nul - pause before exiting. & echo:
+set /p doLoop=what would you like the script to do after generating a CSL? 
+if /i "%doLoop%" EQU "exit" goto :noloop
+if /i "%doLoop%" EQU "loop" goto :loop
+if /i "%doLoop%" EQU "nul" goto :pause
+echo: & echo Invalid response!
+goto :stloop
+:noloop
+set "doLoop=0" & goto :write
+:loop
+set "doLoop=1" & goto :write
+:pause
+set "doLoop=2" & goto :write
+:write
+echo: &  echo =================================================================================== & echo:
+echo REM =================================================================================== > config.cmd
+echo.>> config.cmd
+echo REM CONFIG GENERATED ON %DATE% - %TIME% >> config.cmd
+echo.>> config.cmd
+echo REM =================================================================================== >> config.cmd 
+echo.>> config.cmd
+echo set "browser=%browser%" >> config.cmd
+echo set "doLoop=%doLoop%" >> config.cmd
+echo done. delete config.cmd to change any parameters. & echo restart autocsl to use normally. & pause >nul
